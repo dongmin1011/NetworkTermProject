b752bb99f01a352140d76e873c1f40e601a7433e
dongmin1011 <dmk4660@naver.com>
2023-03-27 13:54:54 +0900

    drink

---
+++ src/main/java/NetwrokTermProject/Drinks/AbstractDrink.java
@@ -0,0 +1,27 @@
+package NetwrokTermProject.Drinks;
+
+public abstract class AbstractDrink {
+    protected String name;	//음료수 이름
+    protected int price;	//가격
+    protected int stock;	//재고
+
+    public AbstractDrink() {
+        stock = 3;
+    }
+//    boolean Stock_IsExist() {
+//        if(stock>0) return true;
+//        else return false;
+//    }
+
+    public String getName() { return name;  }
+    public int getPrice() {  return price; }
+    public int getStock() {  return stock;  }
+
+    public void setName(String name) {  //음료수 이름 변경
+        this.name = name;
+    }
+
+    public void setPrice(int price) {  //음료수 가격 변경
+        this.price = price;
+    }
+}
---
+++ src/main/java/NetwrokTermProject/Drinks/Drink.java
@@ -0,0 +1,9 @@
+package NetwrokTermProject.Drinks;
+
+public class Drink extends AbstractDrink{
+
+    public Drink(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
